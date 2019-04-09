13ca4e0e5f861d1866144397259695ec75c1888f
vagrant <vagrant@localhost.localdomain>
2019-04-10 08:56:34 +0900

    textpage-changed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,13 @@ class HomeController < ApplicationController
   end
 
   def create
+    @title = params[:title]
+    @reason = params[:reason]
+    @story = params[:story]
+    @moving = params[:moving]
+    @why = params[:why]
+    @learn = params[:learn]
+    @action = params[:action]
   end
 
   def text
@@ -14,6 +21,9 @@ class HomeController < ApplicationController
     @why = params[:why]
     @learn = params[:learn]
     @action = params[:action]
+
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/create' => "home#create"
   get '/text' => "home#text"
 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
