32cd1a285f81045f9ac7c8549552af328cc15dc4
vagrant <vagrant@localhost.localdomain>
2019-04-10 05:02:40 +0900

    satoko-test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'question/sheet'
+
   get '/' => "home#top"
-  get "/create" => "home#create"
-  get "/text"  => "home#text"
+
+
+  get "/answer" => "question#sheet"
+  post "/create" => "question#create"
+  get "/text"  => "question#text"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
