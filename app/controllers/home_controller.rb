1390d63bb8e69c747213ef396a3027cc0d320395
Sati2357 <info@medicihiroba.com>
2019-05-08 08:15:57 +0900

    creat text result

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,19 @@
 class HomeController < ApplicationController
+
   def top
   end
 
   def create
   end
+
+  def text
+    @title = params[:title]
+    @reason = params[:reason]
+    @story = params[:story]
+    @moving = params[:moving]
+    @why = params[:why]
+    @learn = params[:learn]
+    @action = params[:action]
+  end
+
 end
